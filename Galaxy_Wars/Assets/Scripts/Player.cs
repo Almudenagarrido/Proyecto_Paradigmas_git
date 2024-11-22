0bea407a81aba795da998ac3c7bfa0a11ee4e512
Almudena <202204789@alu.comilllas.edu>
2024-11-22 16:12:56 +0100

    Nose muy bie que es pero pusheo

--- Galaxy_Wars/Assets/Scripts/Player.cs
+++ Galaxy_Wars/Assets/Scripts/Player.cs
@@ -1,23 +1,15 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float maxSpeed = 20f;
+    public float maxSpeed = 15f;
     private float acceleration = 8f;
     private float deceleration = 1f;
     public int playerNumber;
     private float currentSpeed = 0f;
     private float rotationSpeed = 300f;
     private bool isDead = false;
-    private float topMargin = 5.3f;
-    private float bottomMargin = -5.35f;
-    private float leftMargin = -10.4f;
-    private float rightMargin = 10.4f;
-    private float blackhole1x = 8.1f;
-    private float blackhole1y = -3.5f;
-    private float blackhole2x = -8.1f;
-    private float blackhole2y = 3.5f;
+
 
     private void Update()
     {
@@ -25,8 +17,6 @@ public class Player : MonoBehaviour
         {
             HandleMovement();
             HandleShooting();
-            HandleFrames();
-            HandleBlackholes();
         }
     }
 
@@ -94,50 +84,6 @@ public class Player : MonoBehaviour
         // Aplicar la rotación y movimiento
         transform.Rotate(0f, 0f, rotation);
         transform.Translate(Vector3.up * currentSpeed * Time.deltaTime, Space.Self);
-    }
-
-    private void HandleFrames()
-    {
-        Vector3 currentPosition = transform.position;
-
-        if (currentPosition.y <= bottomMargin)
-        {
-            currentPosition.y = topMargin;
-        }
-        else if (currentPosition.y >= topMargin)
-        {
-            currentPosition.y = bottomMargin;
-        }
-        if (currentPosition.x <= leftMargin)
-        {
-            currentPosition.x = rightMargin;
-        }
-        else if (currentPosition.x >= rightMargin)
-        {
-            currentPosition.x = leftMargin;
-        }
-
-        transform.position = currentPosition;
-    }
-
-    private void HandleBlackholes()
-    {
-        Vector3 currentPosition = transform.position;
-
-        float tolerance = 0.01f;
-
-        if (Mathf.Abs(currentPosition.x - blackhole1x) < tolerance && Mathf.Abs(currentPosition.y - blackhole1y) < tolerance)
-        {
-            currentPosition.x = blackhole2x;
-            currentPosition.y = blackhole2y;
-        }
-        else if (Mathf.Abs(currentPosition.x - blackhole2x) < tolerance && Mathf.Abs(currentPosition.y - blackhole2y) < tolerance)
-        {
-            currentPosition.x = blackhole1x;
-            currentPosition.y = blackhole1y;
-        }
-
-        transform.position = currentPosition;
     }
 
     private void HandleShooting()
