9659510bfe32f11ecadaba8c7b3df5de5e1c4d84
palomadiez <202204513@alu.comillas.edu>
2025-01-02 20:45:49 +0100

    Menú inicio
    Inicio de desarrollo del menú, he aprendido un poco de botones :)

--- Galaxy_Wars/Assets/Scripts/MenuController.cs
+++ Galaxy_Wars/Assets/Scripts/MenuController.cs
@@ -1,25 +1,35 @@
 using UnityEngine;
-using UnityEngine.UI;
+//using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public Dropdown levelDropdown;
-    public Dropdown playersDropdown;
-    public Toggle aiToggle;
-    public Button startButton;
+    //public Dropdown levelDropdown;
+    //public Dropdown playersDropdown;
+    //public Toggle aiToggle;
+    //public Button startButton;
 
     private void Start()
     {
-        startButton.onClick.AddListener(OnStartGame);
+        //startButton.onClick.AddListener(OnStartGame);
+    }
+
+    private void Update()
+    {
+        
     }
 
     private void OnStartGame()
     {
-        int selectedLevel = levelDropdown.value + 1; // Asumiendo 1, 2, 3.
-        int numberOfPlayers = playersDropdown.value + 1; // 1 o 2 jugadores.
-        bool useAI = aiToggle.isOn;
+        //int selectedLevel = levelDropdown.value + 1; // Asumiendo 1, 2, 3.
+        //int numberOfPlayers = playersDropdown.value + 1; // 1 o 2 jugadores.
+        //bool useAI = aiToggle.isOn;
 
-        GameManager.Instance.StartGame(selectedLevel, numberOfPlayers, useAI);
+        //GameManager.Instance.StartGame(selectedLevel, numberOfPlayers, useAI);
     }
-}
+
+    public void PlayButton(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 
